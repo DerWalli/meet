042497fae68798008d7783afbec7385d9e0847cd
DerWalli <33699620+DerWalli@users.noreply.github.com>
2023-02-14 15:28:32 +0100

    TDD - Unit Testing

---
+++ src/Event.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+  state = { collapsed: true };
+  toggleDetails = () => {
+    this.setState(prevState => ({
+      collapsed: !prevState.collapsed
+    }))
+  }
+
+  render() {
+    const { event } = this.props;
+    const { collapsed } = this.state;
+
+    return (
+    <div className="Event">
+      <h2 className="summary">{ event.summary }</h2>
+      <p className="location">{`Location: ${event.location}`}</p>
+      <button 
+        className="details-button"
+        onClick={this.toggleDetails}
+      >{ collapsed ? 'Show' : 'Hide' }Details
+      </button>
+      {!collapsed && (
+        <div className="details">
+          <p className="description">{`Description: ${event.description}`}</p>
+        </div>
+      )}
+    </div>
+  )}
+}
+export default Event;
